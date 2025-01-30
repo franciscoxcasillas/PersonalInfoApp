a14868ef541f8773a78516d201f05296e6d93947
Francisco Casillas <chiccasillas@gmail.com>
2025-01-30 11:14:36 -0600

    Initial Commit

---
+++ PersonalInfoApp/PersonalInfoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersonalInfoAppApp.swift
+//  PersonalInfoApp
+//
+//  Created by Francisco Jean on 30/01/25.
+//
+
+import SwiftUI
+
+@main
+struct PersonalInfoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
