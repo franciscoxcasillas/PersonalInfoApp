2c953cf21429481a18448995f57d3cb0d42007ff
Francisco Casillas <chiccasillas@gmail.com>
2025-01-30 11:37:50 -0600

    Create TitleView with custom font and ProfileImageView with rounded avatar, border, and shadow effects

--- PersonalInfoApp/ContentView.swift
+++ PersonalInfoApp/ContentView.swift
@@ -10,12 +10,31 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            TitleView()
+                .padding(.bottom, 1)
+            ProfileImageView()
         }
-        .padding()
+    }
+}
+
+struct TitleView: View {
+    var body: some View {
+        Text("Fank Sinatra")
+            .font(.custom("Caveat", size: 31))
+            .foregroundColor(.red)
+        
+    }
+}
+
+struct ProfileImageView: View {
+    var body: some View {
+        Image("frank")
+            .resizable()
+            .scaledToFit()
+            .frame(width: 100)
+            .clipShape(Circle())
+            .shadow(radius: 15)
+            .overlay(Circle().stroke(Color.red, lineWidth: 2))
     }
 }
 
